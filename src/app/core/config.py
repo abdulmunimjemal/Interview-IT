1b704c03e4460217a960e00bd6cddbf166b8ccc0
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-08-23 07:29:30 +0300

    [ENH] Added JWT Authentication Utils

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -3,7 +3,11 @@ from pydantic import Field
 
 class Settings(BaseSettings):
     database_url: str = Field(..., env="DATABASE_URL")
+    # JWT
     secret_key: str = Field(..., env="SECRET_KEY")
+    jwt_expires: int = Field(..., env="JWT_EXPIRES")
+    algorithm: str = "HS256"
+    # OAuth
     google_client_id: str = Field(..., env="GOOGLE_CLIENT_ID")
     google_client_secret: str = Field(..., env="GOOGLE_CLIENT_SECRET")
     email_verification_secret: str = Field(..., env="EMAIL_VERIFICATION_SECRET")
