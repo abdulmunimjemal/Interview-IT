9c18241170f1deb0b2c3095b2da73fc0dfc48d84
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-08-23 06:30:39 +0300

    [ENH] Added User CRUD

--- src/app/crud/user.py
+++ src/app/crud/user.py
@@ -1,20 +1,32 @@
-from fastcrud import FastCRUD, crud_router
+from sqlalchemy.orm import Session
 from app.models.user import User
 from app.schemas.user import UserCreate, UserUpdate
-from app.main import get_session
+from app.utils.auth_utils import get_password_hash
 
-# Database session dependency
-async def get_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_session() as session:
-        yield session
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
 
-# Create CRUD router
-user_router = crud_router(
-    schema=User,
-    create_schema=UserCreate,
-    update_schema=UserUpdate,
-    db_model=User,
-    db_session=get_session,
-    prefix="/users",
-    tags=["users"],
-)
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+def create_user(db: Session, user: UserCreate):
+    password = get_password_hash(user.password)
+    db_user = User(
+        name=user.name,
+        email=user.email,
+        password=password
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def update_user(db: Session, user_id: int, user: UserUpdate):
+    db_user = get_user(db, user_id)
+    if user.password:
+        db_user.password = get_password_hash(user.password)
+    db_user.name = user.name
+    db_user.email = user.email
+    db.commit()
+    db.refresh(db_user)
+    return db_user
